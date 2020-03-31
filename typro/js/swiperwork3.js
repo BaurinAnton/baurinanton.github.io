b28f3b4c0af1ec8af8aeaf377f1fce951bfcb3c4
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2020-03-31 19:41:26 +0300

    Upgrade Typro

---
+++ typro/js/swiperwork3.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.workphone', {
+    loop: true,
+    speed: 1000,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+    },
+});
