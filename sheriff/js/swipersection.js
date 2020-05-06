4ad64ed1565bddf3620c05c41e7c6351f4f4561c
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2020-05-07 01:07:52 +0300

    Upgrade Sheriff

---
+++ sheriff/js/swipersection.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    breakpoints: {
+        300: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1100: {
+            slidesPerView: 3,
+        },
+        1500: {
+            slidesPerView: 4,
+        }
+      },
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    direction: getDirection(),
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection());
+      }
+    }
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 1 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
