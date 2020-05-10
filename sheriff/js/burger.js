b6fa2c28dd31f34695640d2f6359fefff5692654
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2020-05-10 15:03:47 +0300

    Update Sheriff

--- sheriff/js/burger.js
+++ sheriff/js/burger.js
@@ -6,3 +6,11 @@ $(document).ready(function() {
         $('.header_menu').toggleClass('header_menu_active');
     });
 });
+$(document).ready(function() {
+    $('.header_menu a').click(function(event) {
+        $('.items_1').toggleClass('items_active1');
+        $('.items_2').toggleClass('items_acrive2');
+        $('.items_3').toggleClass('items_active3');
+        $('.header_menu').toggleClass('header_menu_active');
+    });
+});
