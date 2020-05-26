609e0f99eaeb04110d69a845e36b5fdfbffb2034
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2020-05-26 23:33:17 +0300

    Upgrade Tyzhpro

---
+++ typro/js/burger.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.items_column_burger').click(function(event) {
+        $('.items_1').toggleClass('items_active1');
+        $('.items_2').toggleClass('items_acrive2');
+        $('.items_3').toggleClass('items_active3');
+        $('.header_menu').toggleClass('header_menu_active');
+    });
+});
+$(document).ready(function() {
+    $('.header_menu a').click(function(event) {
+        $('.items_1').toggleClass('items_active1');
+        $('.items_2').toggleClass('items_acrive2');
+        $('.items_3').toggleClass('items_active3');
+        $('.header_menu').toggleClass('header_menu_active');
+    });
+});
