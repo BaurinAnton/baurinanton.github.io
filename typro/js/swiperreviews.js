5c9c59639e16224c86ea7e9f799b62fb8f1c1987
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2020-05-30 22:17:16 +0300

    Update Tyzhpro

---
+++ typro/js/swiperreviews.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper('.reviewsswiper', {
+    breakpoints: {  
+        320: {
+            slidesPerView: 1,
+        },
+        576: {
+            slidesPerView: 1.2,
+          }, 
+          768: {
+            slidesPerView: 1.6,
+          }, 
+          993: {
+            slidesPerView: 1.4,
+          },
+        },
+    spaceBetween: 20,
+    loop: true,
+    zoom: true,
+    centeredSlides: true,
+    speed: 1000,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: true,
+    },
+});
