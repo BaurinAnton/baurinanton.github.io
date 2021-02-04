c347d77c229aa28733f2e3537cce7a6688337341
BaurinAnton <baurinanton2013@yandex.ru>
2021-02-04 22:09:53 +0300

    Update

--- whislove/js/frameshow.js
+++ whislove/js/frameshow.js
@@ -1,9 +1,12 @@
 let sectionFrame = document.getElementById('sectionShow')
 let buttonShow = document.getElementById('buttonFrameShow')
 let removeSection = document.getElementById('sectionHide')
+let overflow = document.getElementById('body')
 buttonShow.onclick = () => {
     sectionFrame.style.display = 'grid'
+    overflow.style.overflow = 'hidden'
 }
 removeSection.onclick = () => {
     sectionFrame.style.display = 'none'
+    overflow.style.overflow = 'auto'
 }
