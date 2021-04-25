dd9f0bcb24c29365328e15ea77ced3b46ab9c462
BaurinAnton <baurinanton2013@yandex.ru>
2021-04-26 02:05:30 +0300

    Upgrade DobryjPar

---
+++ goodSteam/js/trash.js
@@ -0,0 +1,20 @@
+// let quantityOne = document.getElementById('quantity_one')
+// let priceOne = document.getElementById('price_one')
+// const buttonQuantityOne = document.getElementById('button_quantity_one')
+// const buttonQuantityTo = document.getElementById('button_quantity_to')
+// const changingTheAmountOfReduce = (quantityReduction, price) => {
+//     if (quantityReduction.innerHTML > 1) {
+//         +quantityReduction.innerHTML--; price.innerHTML = +price.innerHTML - 18600 
+//     }
+// }
+// const changingTheAmountOfIncrease = (quantityIncrease, price) => {
+//     +quantityIncrease.innerHTML++
+//     price.innerHTML = +price.innerHTML + 18600  
+// }
+// buttonQuantityOne.onclick = () => {
+//     changingTheAmountOfReduce(quantityOne, priceOne)
+// }
+// buttonQuantityTo.onclick = () => {
+//     changingTheAmountOfIncrease(quantityOne, priceOne)
+// }
+
