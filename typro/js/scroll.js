555f68348e0570933c2cb5ba2851ab84f21845c5
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2022-06-27 00:07:59 +0300

    Update

--- typro/js/scroll.js
+++ typro/js/scroll.js
@@ -1,23 +1,23 @@
 var sections = $('section')
   , nav = $('nav')
-  , nav_height = nav.outerHeight();
+  , nav_height = nav.outerHeight() + 55;
 
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
+// $(window).on('scroll', function () {
+//   var cur_pos = $(this).scrollTop();
   
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
+//   sections.each(function() {
+//     var top = $(this).offset().top - nav_height,
+//         bottom = top + $(this).outerHeight();
     
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('nav_active');
-      sections.removeClass('nav_active');
+//     if (cur_pos >= top && cur_pos <= bottom) {
+//       nav.find('a').removeClass('nav_active');
+//       sections.removeClass('nav_active');
       
-      $(this).addClass('nav_active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('nav_active');
-    }
-  });
-});
+//       $(this).addClass('nav_active');
+//       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('nav_active');
+//     }
+//   });
+// });
 
 nav.find('a').on('click', function () {
   var $el = $(this)
