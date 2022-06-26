6d585d3506fa7b191dc63c0236056a12a31ef3a1
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2022-06-26 21:40:50 +0300

    Update scroll.js

--- sheriff/js/scroll.js
+++ sheriff/js/scroll.js
@@ -1,23 +1,6 @@
 var sections = $('section')
   , nav = $('nav')
-  , nav_height = nav.outerHeight();
-
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
-  
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
-    
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('nav_active');
-      sections.removeClass('nav_active');
-      
-      $(this).addClass('nav_active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('nav_active');
-    }
-  });
-});
+  , nav_height = nav.outerHeight() + 10;
 
 nav.find('a').on('click', function () {
   var $el = $(this)
